c21b0ca706dd8c91d1fe415f352dd1ccfa14d325
Tatiana <itstepkashpruk@gmail.com>
2021-09-29 12:34:32 +0300

    learn loop: for, while-do, do-while, continue and break

--- script.js
+++ script.js
@@ -1,31 +1,21 @@
 /* jslint node: true */
 /* jshint browser: true */
 "use strict";
-if (4 == 9){
-  console.log("OK");
-} else {
-  console.log("Error");
-}
+let num = 0;
 
-const num = 89;
-if (num < 49){
-  console.log('Error');
-} else if(num > 100){
-  console.log('A lot!');
-} else{
-  console.log('Ok!');
-}
-const num1 = 50;
-
-(num1 == 50) ? console.log('Ok!') : console.log('Error');
-
-const num2 =30;
-switch(num2) {
-case 50: console.log('Неверно!');
-break;
-case 10: console.log('Неверно!');
-break;
-case 31: console.log('Верно!');
-break;
-default: console.log('Не в этот раз!');
+// while (num <= 55){
+//   num++;
+//   console.log(num);
+// }
+do{
+  console.log(num);
+  num++;
+} while(num < 10);
+let num1 = 0;
+for(let i = 0; i < 10; i++){
+  if(i == 6){
+    // break;
+    continue;
+  }
+console.log(i);
 }
